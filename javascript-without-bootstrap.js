04b20d21e584d3ef7fb16ad6190841c8af0a4435
Isabel-U <39589580+Isabel-U@users.noreply.github.com>
2018-07-04 16:23:48 +0200

    audio file for index

--- javascript-without-bootstrap.js
+++ javascript-without-bootstrap.js
@@ -1,7 +1,42 @@
-var v = document.getElementById('music');
+/*var v = document.getElementById('music');
 
 function muteMusic() {
     v.volume = 0.0;
+}*/
+
+
+
+function VolumeDown() {
+  var v = document.getElementById('music');
+  if ($(window).width() < 870) {
+    v.volume = 0.0;
+  }
+  else {
+    v.volume = 0.1;
+    v.play();
+  }
+
+}
+
+function playPause() {
+  var v = document.getElementById('music');
+  var p = document.getElementById("audio-pause-or-play");
+  if (!music.paused) {
+    v.pause();
+    p.src = "img/play.png"
+  }
+  else {
+    v.play();
+    p.src = "img/pause.png"
+  }
+}
+
+function volumeUp() {
+  document.getElementById("music").volume += 0.1;
+}
+
+function volumeDown() {
+  document.getElementById("music").volume -= 0.1;
 }
 
 /* Funktion Getränke anzeigen */
